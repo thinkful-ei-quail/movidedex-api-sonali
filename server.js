2c1d00ea103d6c86c35c65f6eaa716aca22b5ac8
Sonali Najera <sonalitzul@gmail.com>
2020-08-11 09:21:34 -0700

    server connected

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const express = require('express');
+const morgan = require('morgan');
+//need to require env & helmet & cors later
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use((req, res) => {
+  res.send('Hello, world!');
+});
+
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+})
